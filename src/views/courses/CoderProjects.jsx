b258452e3101b4eb410a72c5fac5ae59efa6f714
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-04-24 14:10:40 -0300

    finalizado projeto

--- src/views/courses/CoderProjects.jsx
+++ src/views/courses/CoderProjects.jsx
@@ -12,7 +12,7 @@ const CoderProjects = props => {
         path='https://github.com/Gonzagadavid/calculator-mac-OS-with-ReactJS'
       />
       <ProjectCard
-        title='Palavras mais faladas com JavaScript Vannila' tec='js'
+        title='Palavras mais faladas com JavaScript Vanilla' tec='js'
         description='Neste projeto foi fornecido as legendas de uma série de televisão e foram organizada as palavras com a quantidade de vezes em que foram pronunciadas e postado em um arquivo json'
         path='https://github.com/Gonzagadavid/most-spoken-words'
       />
