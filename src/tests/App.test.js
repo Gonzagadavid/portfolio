00536ddffd1787ca17fcade459458caaafddb580
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-03 20:49:25 -0300

    desenvolvido testes para a pagina inicial em App.test.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,11 +1,39 @@
+import { screen } from '@testing-library/dom';
 import React from 'react';
 import App from '../App';
+import {
+  COURSES_PROJECTS, OTHER_PROJECTS, PERSONAL_PROJECTS, keyWords, PERFIL_PHOTO,
+} from './constants';
 import renderWithRouter from './helpers/renderWithRouter';
 
-describe('teste rotas e rederizacao da pagina', () => {
+describe('teste rotas e rederizacao da aplicacao', () => {
   window.scrollTo = jest.fn().mockImplementation((x) => x);
-  it('testa url das rotas', () => {
-    const { history } = renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
+
+  const skills = screen.getByRole('link', { name: 'Habilidades' });
+  const personalProjects = screen.getByRole('link', { name: PERSONAL_PROJECTS });
+  const coursesProjects = screen.getByRole('link', { name: COURSES_PROJECTS });
+  const otherProjects = screen.getByRole('link', { name: OTHER_PROJECTS });
+  const imagePerfil = screen.getByAltText(PERFIL_PHOTO);
+
+  it('testa a rota e a rendeirzacao da pagina inicial', () => {
     expect(history.location.pathname).toBe('/');
+
+    expect(skills).toBeInTheDocument();
+    expect(personalProjects).toBeInTheDocument();
+    expect(coursesProjects).toBeInTheDocument();
+    expect(otherProjects).toBeInTheDocument();
+    expect(imagePerfil).toBeInTheDocument();
+
+    const nameTitle = screen.getByRole('heading', { level: 1 });
+
+    expect(nameTitle).toBeInTheDocument();
+    expect(nameTitle).toHaveTextContent('David Gonzaga da Silva');
+
+    keyWords.forEach((word) => {
+      const strongWord = screen.getByText(word);
+      expect(strongWord).toBeInTheDocument();
+      expect(strongWord.tagName).toBe('STRONG');
+    });
   });
 });
---
+++ src/tests/constants/index.js
@@ -0,0 +1,16 @@
+const keyWords = [
+  'São José dos Campos - São Paulo - Brasil', 'era da informação', 'desenvolvimento web',
+  'metologias ágeis das empresas', 'tecnologia',
+];
+
+const PERSONAL_PROJECTS = 'Projetos de iniciativa pessoal';
+
+const COURSES_PROJECTS = 'Projetos desenvolvidos durante cursos';
+
+const OTHER_PROJECTS = 'Outros projetos';
+
+const PERFIL_PHOTO = 'foto de David';
+
+export {
+  keyWords, PERSONAL_PROJECTS, COURSES_PROJECTS, OTHER_PROJECTS, PERFIL_PHOTO,
+};
