a039d5a1ffabcc7c03822e6471589ce90fbc9878
Gonzagadavid <gonzagadaviddev@gmail.com>
2021-09-04 22:22:25 -0300

    desenvolvido testes para as paginas de projetos pessoais, projetos desenvolvido durante cursos e outros projetos

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,14 +1,19 @@
-import { fireEvent, screen } from '@testing-library/dom';
+import { fireEvent, screen, waitFor } from '@testing-library/dom';
 import { cleanup } from '@testing-library/react';
 import React from 'react';
 import App from '../App';
 import { hardSkills, softSkills } from '../data/lists/skillsList';
+import otherProjects from '../data/projects/otherProjects';
+import personalProjects from '../data/projects/personalProjects';
 import {
-  COURSES_PROJECTS, OTHER_PROJECTS, PERSONAL_PROJECTS, keyWords, PERFIL_PHOTO, softSkillsTitle, hardSkillsTitle,
+  COURSES_PROJECTS, OTHER_PROJECTS, PERSONAL_PROJECTS, keyWords, PERFIL_PHOTO, softSkillsTitle,
+  hardSkillsTitle, NOME_TITLE, courses,
 } from './constants';
 import renderWithRouter from './helpers/renderWithRouter';
 
-describe('teste rotas e rederizacao da pagina inicial e a de habilidades', () => {
+afterEach(cleanup);
+
+describe('teste rotas e rederizacao da aplicacao', () => {
   window.scrollTo = jest.fn().mockImplementation((x) => x);
 
   const checkList = (list) => list.forEach((itemList) => {
@@ -18,28 +23,38 @@ describe('teste rotas e rederizacao da pagina inicial e a de habilidades', () =>
     expect(item.parentElement.tagName).toBe('UL');
   });
 
+  const checkProjects = (projectList) => projectList.forEach(({ title, description }) => {
+    const projectTile = screen.getByText(title);
+    const projectDescription = screen.getByText(description);
+
+    expect(projectTile).toBeInTheDocument();
+    expect(projectTile.tagName).toBe('H4');
+    expect(projectDescription).toBeInTheDocument();
+    expect(projectDescription.tagName).toBe('EM');
+  });
+
   afterEach(cleanup);
   it('testa a rota e a rendeirzacao da pagina inicial', () => {
     const { history } = renderWithRouter(<App />);
 
     const skills = screen.getByRole('link', { name: 'Habilidades' });
-    const personalProjects = screen.getByRole('link', { name: PERSONAL_PROJECTS });
+    const personalProjectsLink = screen.getByRole('link', { name: PERSONAL_PROJECTS });
     const coursesProjects = screen.getByRole('link', { name: COURSES_PROJECTS });
-    const otherProjects = screen.getByRole('link', { name: OTHER_PROJECTS });
+    const otherProjectsLink = screen.getByRole('link', { name: OTHER_PROJECTS });
     const imagePerfil = screen.getByAltText(PERFIL_PHOTO);
 
     expect(history.location.pathname).toBe('/');
 
     expect(skills).toBeInTheDocument();
-    expect(personalProjects).toBeInTheDocument();
+    expect(personalProjectsLink).toBeInTheDocument();
     expect(coursesProjects).toBeInTheDocument();
-    expect(otherProjects).toBeInTheDocument();
+    expect(otherProjectsLink).toBeInTheDocument();
     expect(imagePerfil).toBeInTheDocument();
 
     const nameTitle = screen.getByRole('heading', { level: 1 });
 
     expect(nameTitle).toBeInTheDocument();
-    expect(nameTitle).toHaveTextContent('David Gonzaga da Silva');
+    expect(nameTitle).toHaveTextContent(NOME_TITLE);
 
     keyWords.forEach((word) => {
       const strongWord = screen.getByText(word);
@@ -52,18 +67,9 @@ describe('teste rotas e rederizacao da pagina inicial e a de habilidades', () =>
     const { history } = renderWithRouter(<App />);
 
     const skills = screen.getByRole('link', { name: 'Habilidades' });
-    const personalProjects = screen.getByRole('link', { name: PERSONAL_PROJECTS });
-    const coursesProjects = screen.getByRole('link', { name: COURSES_PROJECTS });
-    const otherProjects = screen.getByRole('link', { name: OTHER_PROJECTS });
-    const imagePerfil = screen.getByAltText(PERFIL_PHOTO);
 
     expect(history.location.pathname).toBe('/');
-
     expect(skills).toBeInTheDocument();
-    expect(personalProjects).toBeInTheDocument();
-    expect(coursesProjects).toBeInTheDocument();
-    expect(otherProjects).toBeInTheDocument();
-    expect(imagePerfil).toBeInTheDocument();
 
     fireEvent.click(skills);
 
@@ -80,5 +86,70 @@ describe('teste rotas e rederizacao da pagina inicial e a de habilidades', () =>
 
     checkList(softSkills);
     checkList(hardSkills);
+  });
+
+  it('verifica a renderizacao da pagina de projetos pessoais', async () => {
+    const { history } = renderWithRouter(<App />);
+    const { titleCourse, projects } = personalProjects;
+
+    expect(history.location.pathname).toBe('/');
+
+    const personalProjectsLink = screen.getByRole('link', { name: PERSONAL_PROJECTS });
+    expect(personalProjectsLink).toBeInTheDocument();
+
+    fireEvent.click(personalProjectsLink);
+
+    await waitFor(() => expect(history.location.pathname).toBe('/projetos_pessoais'));
+
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(titleCourse);
+
+    checkProjects(projects);
+  });
+
+  it('verifica a renderizacao da pagina de projetos feitos durante o curso', () => {
+    const { history } = renderWithRouter(<App />);
+
+    expect(history.location.pathname).toBe('/');
+
+    const coursesProjects = screen.getByRole('link', { name: COURSES_PROJECTS });
+    expect(coursesProjects).toBeInTheDocument();
+
+    fireEvent.click(coursesProjects);
+
+    expect(history.location.pathname).toBe('/projetos_cursos');
+
+    courses.forEach(async ({ linkCourse, projectsCourse, path }) => {
+      const link = screen.getByText(linkCourse);
+      expect(link.tagName).toBe('A');
+      expect(link).toBeInTheDocument();
+
+      fireEvent.click(link);
+
+      await waitFor(() => expect(history.location.pathname).toBe(path));
+
+      checkProjects(projectsCourse);
+    });
+  });
+
+  it('verifica a renderizacao da pagina de outros projetos', async () => {
+    const { history } = renderWithRouter(<App />);
+    const { titleCourse, projects } = otherProjects;
+
+    expect(history.location.pathname).toBe('/');
+
+    const otherProjectsLink = screen.getByRole('link', { name: OTHER_PROJECTS });
+    expect(otherProjectsLink).toBeInTheDocument();
+
+    fireEvent.click(otherProjectsLink);
+
+    await waitFor(() => expect(history.location.pathname).toBe('/outros_projetos'));
+
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toBeInTheDocument();
+    expect(title).toHaveTextContent(titleCourse);
+
+    checkProjects(projects);
   });
 });
--- src/tests/constants/index.js
+++ src/tests/constants/index.js
@@ -1,3 +1,9 @@
+import cod3rProjects from '../../data/projects/cod3rProjects';
+import trybeProjects from '../../data/projects/trybeProjects';
+import serFrontendProjects from '../../data/projects/serFrontendProjects';
+import santProjects from '../../data/projects/santProjects';
+import dioProjects from '../../data/projects/dioProjects';
+
 const keyWords = [
   'São José dos Campos - São Paulo - Brasil', 'era da informação', 'desenvolvimento web',
   'metologias ágeis das empresas', 'tecnologia',
@@ -14,7 +20,17 @@ const OTHER_PROJECTS = 'Outros projetos';
 
 const PERFIL_PHOTO = 'foto de David';
 
+const NOME_TITLE = 'David Gonzaga da Silva';
+
+const courses = [
+  { linkCourse: 'Trybe', projectsCourse: trybeProjects, path: '/projetos_cursos/projetos-trybe' },
+  { linkCourse: 'Cod3r', projectsCourse: cod3rProjects, path: '/projetos_cursos/projetos-coder' },
+  { linkCourse: 'SerFrontend', projectsCourse: serFrontendProjects, path: '/projetos_cursos/projetos-serfrentend' },
+  { linkCourse: "Sant'Ana", projectsCourse: santProjects, path: '/projetos_cursos/projetos-santana' },
+  { linkCourse: 'DIO', projectsCourse: dioProjects, path: '/projetos_cursos/projetos-dio' },
+];
+
 export {
   keyWords, PERSONAL_PROJECTS, COURSES_PROJECTS, OTHER_PROJECTS, PERFIL_PHOTO,
-  softSkillsTitle, hardSkillsTitle,
+  softSkillsTitle, hardSkillsTitle, NOME_TITLE, courses,
 };
